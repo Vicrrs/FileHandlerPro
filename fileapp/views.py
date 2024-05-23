1bf72a9fc6b66ca6b8094162e22ee88cdc8c08f1
Vicrrs <enricofermi2019@gmail.com>
2024-05-23 01:30:20 -0400

    views upload file download file

--- fileapp/views.py
+++ fileapp/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.http import require_http_methods
+from .models import FileUpload
 
-# Create your views here.
+
+@require_http_methods(["POST"])
+def upload_file(request):
+    file_obj = request.FILES['file']
+    file_instance = FileUpload(file=file_obj)
+    file_instance.save()
+    return HttpResponse("File uploaded successfully.")
+
+
+def download_file(request, file_id):
+    file_instance = FileUpload.objects.get(id=file_id)
+    response = HttpResponse(file_instance.file, content_type='application/octet-stream')
+    response['Content-Disposition'] = 'attachment; filename="%s"' % file_instance.file.name
+    return response
