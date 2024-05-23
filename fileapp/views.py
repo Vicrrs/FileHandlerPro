40cca7c9f9a2ff03d3d3fd3a055305f4f1422c92
Vicrrs <enricofermi2019@gmail.com>
2024-05-23 01:47:21 -0400

    funcao para fazer upload de arquivo usando o form

--- fileapp/views.py
+++ fileapp/views.py
@@ -1,6 +1,8 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.http import require_http_methods
 from .models import FileUpload
+from .forms import FileUploadForm
 
 
 @require_http_methods(["POST"])
@@ -11,8 +13,23 @@ def upload_file(request):
     return HttpResponse("File uploaded successfully.")
 
 
+def upload_file_form(request):
+    if request.method == 'POST':
+        form = FileUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return render(request, 'fileapp/upload_file.html', {
+                'form': form,
+                'message': 'File uploaded successfully!'
+            })
+    else:
+        form = FileUploadForm()
+    return render(request, 'fileapp/upload_file.html', {'form': form})
+
+
 def download_file(request, file_id):
     file_instance = FileUpload.objects.get(id=file_id)
-    response = HttpResponse(file_instance.file, content_type='application/octet-stream')
+    response = HttpResponse(
+        file_instance.file, content_type='application/octet-stream')
     response['Content-Disposition'] = 'attachment; filename="%s"' % file_instance.file.name
     return response
